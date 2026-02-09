a5d173559739bc9e20dee067225d0d7f547b3979
Youssef Atef <joeatef769@gamil.com>
2026-02-09 12:48:05 +0200

    ss

--- app/~offline/page.tsx
+++ app/~offline/page.tsx
@@ -1,8 +1,8 @@
 const page = () => {
   return (
-    <div className="w-full flex flex-col gap-1 justify-center items-center h-screen">
-      <h1>Hello again 👋</h1>
-      <p className="">You are currently offline, please check your internet!</p>
+    <div className='w-full flex flex-col gap-1 justify-center items-center h-screen'>
+      <h1>Hello again 👋 👋 👋</h1>
+      <p className=''>You are currently offline, please check your internet!</p>
     </div>
   );
 };
